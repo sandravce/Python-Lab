f824b4a87ed3869d4c4f7902230a6658a56aa36f
sandravce <sandravismaya@gmail.com>
2025-09-23 15:00:03 +0530

    Delete python directory

--- python/greater than 100 , print over.py
+++
@@ -1,8 +0,0 @@
-#prompt the user for a list of inmtegers for all values greater than 100, store "over" instead..
-
-num=int(input("Enter numbers:"))
-for i in num:
-    if i>100:
-        print("over")
-    else:
-        print(i)
--- python/oddeven.py
+++
@@ -1,8 +0,0 @@
-print("ODD OR EVEN\n")
-
-a=int(input("Enter the number:"))
-if a%2==0:
-    print("It is an even number")
-else:
-    print("It is an odd number")
-    
